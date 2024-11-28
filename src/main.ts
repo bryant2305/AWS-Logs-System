e063b53a95ce6e92e2c6c3d357a4e678bf0f47a5
Bryant Perez Garcia <bryantperezgarcia005@gmail.com>
2024-11-28 11:27:49 -0400

    FEAT swagger implementation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Fitness Api')
+    .setDescription('An Api FOR HEALTY')
+    .setVersion('1.0')
+    .build();
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(configService.get<number>('PORT') || 0);
 }
 bootstrap();
