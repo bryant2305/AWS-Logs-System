fcf60b6153ae5b785f85068dee671f65b4f172dc
bryant2305 <bryantperezgarcia005@gmail.com>
2025-07-16 15:26:22 -0400

    feat: implement health check endpoint returning "pong!!"

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('docs')
-  getDocsDebug() {
-    return { message: 'Swagger docs route is accessible!' };
+  @Get('health-check')
+  healthCheck() {
+    return { message: 'pong!!' };
   }
 }
