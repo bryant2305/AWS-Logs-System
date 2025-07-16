6dbf6df68cc8976fbcea920eefd310077639f30e
Bryant Perez Garcia <119883367+bryant2305@users.noreply.github.com>
2025-07-16 15:20:34 -0400

    Update dynamodb.module.ts

--- src/modules/dynamodb/dynamodb.module.ts
+++ src/modules/dynamodb/dynamodb.module.ts
@@ -9,23 +9,6 @@ import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
       useFactory: () => {
         const client = new DynamoDBClient({
           region: 'us-east-2',
-          ...(process.env.AWS_ACCESS_KEY_ID &&
-          process.env.AWS_SECRET_ACCESS_KEY &&
-          process.env.AWS_SESSION_TOKEN
-            ? {
-                credentials: {
-                  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-                  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-                  sessionToken: process.env.AWS_SESSION_TOKEN,
-                },
-              }
-            : {
-                // Si no hay credenciales temporales, usa las credenciales por defecto
-                credentials: {
-                  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-                  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-                },
-              }),
         });
 
         return DynamoDBDocumentClient.from(client);
